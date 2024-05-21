c64ab0abc7d2c710ecb123d55c6780fc10ccb970
GIDEON NSOWAH <giddyohene5@gmail.com>
2024-05-21 20:04:27 +0000

    finished navbar and its responsiveness

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./Home.css";
+import Navbar from '../../components/navigation/NavBar';
+
+function Home() {
+  return (
+    <>
+      <Navbar />
+    </>
+  )
+}
+
+export default Home
